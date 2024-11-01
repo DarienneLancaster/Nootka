bdb01d27fe5070199d5ad192a7673b78974c18a6
DarienneLancaster <101891701+DarienneLancaster@users.noreply.github.com>
2024-11-01 10:56:02 -0700

    added descriptor to beginning of code

--- scripts/1_install_packages.R
+++ scripts/1_install_packages.R
@@ -1,3 +1,6 @@
+#Run this script before any others
+#creates a function to load and install any required packages
+
 #load packages script
 lp<-function(pck){
   if(!require(pck,character.only = TRUE))install.packages(pck);library(pck,character.only = TRUE)
